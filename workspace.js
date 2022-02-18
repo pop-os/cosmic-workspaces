1e34b7f0b038661676894c036fdee09065dd683c
Cereal-Killa <25939765+Cereal-Killa@users.noreply.github.com>
2022-02-18 07:06:52 -0300

    Revert "Minimal update to fix the glitch."
    This reverts commit 29f80357fe95ebb6c3e671cbbb1a9b3f5b262b4f.

--- workspace.js
+++ workspace.js
@@ -21,6 +21,10 @@ const _Util = Self.imports.util;
 const animateAllocation = imports.ui.workspace.animateAllocation;
 
 function updateStaticBackgrounds() {
+    for (var bg of global.vertical_overview.bgManagers) {
+        bg.destroy();
+    }
+
     for (var monitor of Main.layoutManager.monitors) {
         let bgManager = new Background.BackgroundManager({
             monitorIndex: monitor.index,
